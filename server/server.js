844d61253ef5abd5e7c50704a03889d0881f10c0
DC <deepchawla.1993@gmail.com>
2018-01-03 20:19:45 +0530

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.get('/todos/:id', (req, res) => {
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
 
-  if(!ObjectID.isValid){
+  if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
 
