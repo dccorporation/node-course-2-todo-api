f3d8a22b4c7547f5d2dfb28ba27f555f87e504b7
DC <deepchawla.1993@gmail.com>
2018-01-07 23:26:04 +0530

    Add tests for POSTS/ user login

--- server/server.js
+++ server/server.js
@@ -128,6 +128,7 @@ app.post('/users/login', (req, res) => {
     user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
     });
+    
   }).catch((e) => {
     res.status(400).send();
   });
